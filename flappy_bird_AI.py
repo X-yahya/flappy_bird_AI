16d154160ddb1bbaba552e43cba4cf735218e173
lyt <124516380+X-yahya@users.noreply.github.com>
2024-08-25 11:28:04 +0100

    base,pipe classes

--- flappy_bird_AI.py
+++ flappy_bird_AI.py
@@ -68,7 +68,66 @@ class Bird :
     def get_mask(self): 
         return pygame.mask.from_surface(self.img)
     
+class Pipe : 
+    GAP = 200 
+    VEL = 5
 
+    def __init__(self,x):
+        self.x = x 
+        self.height = 0
+        #self.gap = 200
+        self.top = 0 
+        self.bottom = 0 
+        self.PIPE_TOP = pygame.transform.flip(pipe_img , False , False)
+        self.PIPE_BOTTOM = pipe_img
+        self.passed = False
+        self.set_height()
+        
+    def set_height(self):
+        self.height= random.randrange(50,450)
+        self.top=self.height-self.PIPE_TOP.get_height()
+        self.botton = self.height+self.GAP
+    def move(self):
+        self.x -=self.VEL
+    def draw(self,win):
+        win.blit(self.PIPE_TOP , (self.x,self.top))
+        win.blit(self.PIPE_BOTTOM,(self.x,self.bottom))
+    def collide(self,bird,win):
+        bird_mask = bird.get_mask()
+        
+        top_mask =  pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+    
+        top_offset = (self.x-bird.x,self.top-round(bird.y))
+        bottom_offset = (self.x-bird.x,self.bottom-round(bird.y))
+        b_point = bird_mask.overlap(bottom_mask,bottom_offset)
+        t_point = bird_mask.overlap(top_mask,top_offset)
+
+        if t_point or b_point : 
+            return True
+        return False
+
+class base :
+    VEL = 5 
+    WIDTH = base_img.get_width()
+    IMG = base_img 
+
+    def __init__(self,y):
+        self.y = y 
+        self.x1 = 0 
+        self.x2 = self.WIDTH
+        
+    def move(self) : 
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0 : 
+            self.x1 = self.x2 + self.WIDTH
+        if self.x2+self.WIDTH <0 :
+            self.x2 = self.x1 + self.WIDTH
+    def draw(self , win): 
+        win.blit(self.IMG,(self.x1,self.y))
+        win.blit(self.IMG,(self.x2,self.y))
 def draw_window(win , bird): 
     win.blit(bg_img,(0,0))
     bird.draw(win)
@@ -87,7 +146,7 @@ def main() :
                 run = False 
         bird.move()
         draw_window(win , bird) 
-    pygame.qiut()
+    pygame.quit()
     quit()
 
 main()
